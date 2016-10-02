9d37b65e8ed82471fd9c418630b021c7d43b69be
User-PC\User <jimhou28@gmail.com>
2016-10-02 16:38:06 +0800

    用 replaceAll() 把it、Cannot 改成 "her"、can't

--- src/fcu/iecs/oop/Hello.java
+++ src/fcu/iecs/oop/Hello.java
@@ -5,7 +5,20 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello FCU");
+		int num1,num2,num3;
+		
+		String Her="\"her\"";
+		
+		String cant="Can\'t";
+		
+		String lyrics="Let it go! Let it go! Cannot hold it back anymore";
+		
+		
+		//lyrics.replaceAll("it", Her);//不知道為甚麼不能直接用
+		
+		System.out.println(lyrics.replaceAll("it", Her).replaceAll("Cannot",cant));
+		
+		//System.out.println("Hello FCU");
 
 	}
 
