53ccadb91075dcc318a6ffd1d7aab922e6129516
User-PC\User <jimhou28@gmail.com>
2016-10-03 19:57:57 +0800

    replace執行結果=replaceAll，作業有兩總作法

--- src/fcu/iecs/oop/Hello.java
+++ src/fcu/iecs/oop/Hello.java
@@ -4,19 +4,27 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		int num1,num2,num3;
-		
-		String Her="\"her\"";
+			
+		//方法一
+		/*String Her="\"her\"";
 		
 		String cant="Can\'t";
 		
+		System.out.println(lyrics.replaceAll("it", Her).replaceAll("Cannot",cant));*/
+		
+		
+		
+		//方法二
 		String lyrics="Let it go! Let it go! Cannot hold it back anymore";
 		
+		lyrics=lyrics.replace("it","\"her\"" );//replaceAll=replace
 		
-		//lyrics.replaceAll("it", Her);//不知道為甚麼不能直接用
+		lyrics=lyrics.replace("Cannot","Can\'t");
 		
-		System.out.println(lyrics.replaceAll("it", Her).replaceAll("Cannot",cant));
+		System.out.println(lyrics);
+		
+		
+		
 		
 		//System.out.println("Hello FCU");
 
