790bde5d7ca3ff419aebd90bd7c6fecfe45a45b4
User-PC\User <jimhou28@gmail.com>
2016-10-05 07:51:48 +0800

    為String作補充

--- src/fcu/iecs/oop/Hello.java
+++ src/fcu/iecs/oop/Hello.java
@@ -17,9 +17,9 @@ public class Hello {
 		//方法二
 		String lyrics="Let it go! Let it go! Cannot hold it back anymore";
 		
-		lyrics=lyrics.replace("it","\"her\"" );//replaceAll=replace
+		lyrics=lyrics.replace("it","\"her\"" );//replaceAll=replace，而String是在記憶體改建一個新的字串
 		
-		lyrics=lyrics.replace("Cannot","Can\'t");
+		lyrics=lyrics.replace("Cannot","Can\'t");//所以必須重新賦予 lyrics才會改變 lyrics內的值
 		
 		System.out.println(lyrics);
 		
